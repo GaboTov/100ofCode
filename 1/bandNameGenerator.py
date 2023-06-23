66409a71817e6aa6e5775f6d268427f7441de684
Gabo Tovar <gabotovar7@gmail.com>
2023-06-23 15:59:38 +0200

    day 1

---
+++ 1/bandNameGenerator.py
@@ -0,0 +1,13 @@
+
+
+print("""Welcome to Band name generator""")
+
+city = input("What`s the name of the city you grew up in? \n")
+pet_name = input("What's your pet's name \n")
+
+print ("Your band name could be " + city + " " + pet_name)
+
+
+for i in range(100):
+    i +=1
+    print("day", i ,"<br>")
